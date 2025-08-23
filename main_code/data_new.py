711ff5ee03f62e8d5d9df38b32d50f5ffa90087f
Navya-146 <navya2201@gmail.com>
2025-08-23 23:01:08 +0530

    Update data_new.py

--- main_code/data_new.py
+++ main_code/data_new.py
@@ -8,22 +8,19 @@ from tokenizers import Tokenizer
 from omegaconf import OmegaConf
 import os
 
-PROJECT_PATH = "/home/da24c011/miniconda3/project/"
+PROJECT_PATH = "/home/workspace/da24c011"
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # folder where this script lives
-# Your imports for FGR model and get_representation
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 from main_code.FGR.load_FGR import get_fgr_model, get_representation
 
-# -----------------------
 # Dataset
-# -----------------------
 class DrugOmicsIC50Dataset(Dataset):
     def __init__(self, df, drug_encoder, omics_data, drug_dict, tokenizer, 
                  fgroups_list, omics_input_sizes, task="regression",
                  ic50_threshold=None):
         self.df = df.reset_index(drop=True)
         self.encoder = drug_encoder.eval()
-        self.omics = omics_data  # dict of modality_name -> dataframe or array
+        self.omics = omics_data 
         self.drugs = {str(k).strip().lower(): v for k, v in drug_dict.items()}
         self.tokenizer = tokenizer
         self.fgroups_list = fgroups_list
@@ -37,7 +34,6 @@ class DrugOmicsIC50Dataset(Dataset):
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
 
-        # ---- Drug encoding ----
         drug_name = str(row["DRUG_NAME"]).strip().lower()
         if drug_name not in self.drugs:
             raise KeyError(f"Drug '{drug_name}' not found in drug_dict.")
@@ -45,7 +41,6 @@ class DrugOmicsIC50Dataset(Dataset):
 
         drug_rep = get_representation([smiles], "FGR", self.fgroups_list, self.tokenizer)
 
-        # ---- Omics encoding (separate branches) ----
         omics_views = []
         for size, modality in zip(self.omics_input_sizes, ["exp", "cnv", "mut", "meth"]):
             if modality not in self.omics:
@@ -54,7 +49,7 @@ class DrugOmicsIC50Dataset(Dataset):
             omics_vector = torch.tensor(list(omics_dict.values()), dtype=torch.float32)
             omics_views.append(omics_vector)
 
-        # ---- Target ----
+        # Target
         if self.task == "regression":
             target = torch.tensor(row["LN_IC50"], dtype=torch.float32)
         elif self.task == "classification":
@@ -66,13 +61,7 @@ class DrugOmicsIC50Dataset(Dataset):
         
         return torch.from_numpy(drug_rep).float(), *omics_views, target
 
-
-
-
-
-# -----------------------
 # DataModule
-# -----------------------
 class DrugOmicsDataModule(LightningDataModule):
     def __init__(self, data_dir, drug_encoder, train_val_test_split, batch_size, num_workers, task="regression", ic50_threshold=None):
         super().__init__()
@@ -83,43 +72,31 @@ class DrugOmicsDataModule(LightningDataModule):
         self.task = task
         self.threshold = ic50_threshold
 
-        # Load omics data once here
         self.omics = {}
-        self.gene_orders = {}
 
-        # Load expression
         df_expr = pd.read_csv(((os.path.join(PROJECT_PATH, "raw data from colab", "expression_wo_cosmic.csv"))), index_col=0)
         self.omics["expr"] = df_expr.to_dict(orient="index")
-        self.gene_orders["expr"] = list(df_expr.columns)
 
-        # Load mutation
         df_mut = pd.read_csv(((os.path.join(PROJECT_PATH, "raw data from colab", "mutation_wo_cosmic.csv"))), index_col=0)
         self.omics["mut"] = df_mut.to_dict(orient="index")
-        self.gene_orders["mut"] = list(df_mut.columns)
 
-        # Load methylation
         df_meth = pd.read_csv(((os.path.join(PROJECT_PATH, "raw data from colab", "methylation_wo_mean_cosmic.csv"))), index_col=0)
         self.omics["meth"] = df_meth.to_dict(orient="index")
-        self.gene_orders["meth"] = list(df_meth.columns)
 
-        # Load CNV
         df_cnv = pd.read_csv(((os.path.join(PROJECT_PATH, "raw data from colab", "cnv_wo_cosmic.csv"))), index_col=0)
         self.omics["cnv"] = df_cnv.to_dict(orient="index")
-        self.gene_orders["cnv"] = list(df_cnv.columns)
 
-        # Load drug SMILES dictionary
+        # SMILES dictionary
         drug_smiles_df = pd.read_csv((os.path.join(PROJECT_PATH, "data to be used", "drug_smiles.csv")), sep="\t", index_col=0)
         self.drug_dict = drug_smiles_df["SMILES"].to_dict()
 
-        # Load functional groups and tokenizer
+        # functional groups and tokenizer
         fgroups = pd.read_parquet((os.path.join(BASE_DIR, "fg.parquet")))["SMARTS"].tolist()
         self.fgroups_list = [MolFromSmarts(x) for x in fgroups]
         self.tokenizer = Tokenizer.from_file((os.path.join(BASE_DIR,"tokenizers", "BPE_pubchem_500.json")))
 
-        # Load full dataframe with drug, cell line, IC50
         self.full_df = pd.read_csv((os.path.join(PROJECT_PATH, "data to be used", "fin_ic50_21-7.csv")))
 
-        # Train/val/test split proportions
         self.split = train_val_test_split
 
         self.train_set = None
@@ -146,42 +123,10 @@ class DrugOmicsDataModule(LightningDataModule):
 
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers or 0,  persistent_workers=False,pin_memory=False)
 
     def val_dataloader(self):
-        return DataLoader(self.valid_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+        return DataLoader(self.valid_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers or 0,  persistent_workers=False,pin_memory=False)
 
     def test_dataloader(self):
-        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
-
-
-# -----------------------
-# Usage example
-# -----------------------
-if __name__ == "__main__":
-    data_dir = "final_ish/data to be used"
-    batch_size = 32
-    num_workers = 0
-    split = {"train": 0.7, "val": 0.15, "test": 0.15}
-
-    # Load pretrained FGR encoder
-    encoder = get_fgr_model("final_ish/checkpoints/epoch_000_val_0.8505.ckpt")
-
-    # Instantiate DataModule
-    data_module = DrugOmicsDataModule(data_dir, encoder, split, batch_size, num_workers)
-
-    # Prepare datasets
-    data_module.setup()
-
-    # Access loaders
-    train_loader = data_module.train_dataloader()
-    val_loader = data_module.val_dataloader()
-    test_loader = data_module.test_dataloader()
-
-    # Iterate one batch to test
-    for batch in train_loader:
-        drug_vec, omics_views, label = batch
-        print("Drug vector shape:", drug_vec.shape)
-        print("Number of omics views:", len(omics_views))
-        print("Label shape:", label.shape)
-        break
+        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers or 0,  persistent_workers=False,pin_memory=False)
